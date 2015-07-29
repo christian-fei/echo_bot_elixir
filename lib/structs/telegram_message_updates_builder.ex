d353acb6876bcb8c43428f8cf784b97203ebbd62
Christian Fei <crifei93@gmail.com>
2015-07-30 01:54:41 +0200

    added test for telegram message updates builder to get latest update id

--- lib/structs/telegram_message_updates_builder.ex
+++ lib/structs/telegram_message_updates_builder.ex
@@ -4,6 +4,18 @@ defmodule TelegramMessageUpdatesBuilder do
       %GetUpdatesResponse{ok: "false", result: _} -> nil
       %GetUpdatesResponse{ok: "true", result: []} ->
         %TelegramMessageUpdates{latest_update_id: nil, messages: []}
+      %GetUpdatesResponse{ok: "true", result: result} ->
+        latest_update_id = get_latest_update_id result
+        messages = []
+        %TelegramMessageUpdates{latest_update_id: latest_update_id, messages: messages}
     end
   end
+
+  defp get_latest_update_id([]), do: nil
+
+  defp get_latest_update_id(results) do
+    [h|_] = Enum.reverse results
+    h["update_id"]
+  end
+
 end
--- test/telegram_get_updates_to_model_test.exs
+++ test/telegram_get_updates_to_model_test.exs
@@ -18,4 +18,32 @@ defmodule TelegramGetUpdatesToModelTest do
     assert result != nil
     assert result == expected
   end
+
+  test "returns GetUpdatesResponse with messages if can convert GetUpdatesResponse" do
+    getUpdatesResponse = %GetUpdatesResponse{ok: "true", result: [%{
+      "message" => %{
+        "chat" => %{
+          "first_name" => "cf",
+          "id" => 27922851,
+          "username" => "cfcfcfcf"
+        },
+        "date" => 1438213716,
+        "from" => %{
+          "first_name" => "cf",
+          "id" => 27922851,
+          "username" => "cfcfcfcf"
+        },
+        "message_id" => 35,
+        "text" => "/echo test"
+      },
+      "update_id" => 968217487
+    }]}
+
+    result = TelegramMessageUpdatesBuilder.buildWith getUpdatesResponse
+    expected = %TelegramMessageUpdates{latest_update_id: 968217487, messages: []}
+
+    assert result != nil
+    assert result == expected
+  end
+
 end
