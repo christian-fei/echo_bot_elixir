896fedbceca0cf7485517880e0f304b3bdb704b0
Christian Fei <crifei93@gmail.com>
2015-07-30 02:06:15 +0200

    improved test to map to message in telegram message updates builder

--- lib/structs/telegram_message_updates_builder.ex
+++ lib/structs/telegram_message_updates_builder.ex
@@ -6,10 +6,20 @@ defmodule TelegramMessageUpdatesBuilder do
         %TelegramMessageUpdates{latest_update_id: nil, messages: []}
       %GetUpdatesResponse{ok: "true", result: result} ->
         latest_update_id = get_latest_update_id result
-        messages = []
+        messages = mapToMessages result
         %TelegramMessageUpdates{latest_update_id: latest_update_id, messages: messages}
     end
   end
+
+
+  defp mapToMessages [] do
+    []
+  end
+
+  defp mapToMessages [head | tail] do
+    [%{"text" => head["message"]["text"], "update_id" => head["update_id"]} | mapToMessages tail]
+  end
+
 
   defp get_latest_update_id([]), do: nil
 
