f500d274c3fdb7e3e9794d04d760a21ac35e448c
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-08-06 22:31:14 +0200

    refactoring methods

--- lib/telegram_api.ex
+++ lib/telegram_api.ex
@@ -1,9 +1,19 @@
 defmodule TelegramApi do
 
+  @token Application.get_env(:echo_bot_elixir, :telegram_api_token)
+
   def get_updates offset do
-    token = Application.get_env(:echo_bot_elixir, :telegram_api_token)
-    url = "https://api.telegram.org/bot#{token}/getUpdates?offset=#{offset}"
-    case HTTPoison.get(url) do
+    url = "https://api.telegram.org/bot#{@token}/getUpdates?offset=#{offset}"
+    do_apply fn -> HTTPoison.get(url) end
+  end
+
+  def send_message chat_id, text do
+    url = "https://api.telegram.org/bot#{@token}/sendMessage"
+    do_apply fn -> HTTPoison.post(url, {:form, [chat_id: chat_id, text: text]}, %{"Content-type": "application/x-www-form-urlencoded"}) end
+  end
+
+  defp do_apply fun do
+    case fun.() do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         {:ok, body}
       {:ok, %HTTPoison.Response{status_code: 404}} ->
@@ -12,18 +22,4 @@ defmodule TelegramApi do
         {:error, reason}
     end
   end
-
-  def send_message chat_id, text do
-    token = Application.get_env(:echo_bot_elixir, :telegram_api_token)
-    url = "https://api.telegram.org/bot#{token}/sendMessage"
-    case HTTPoison.post(url, {:form, [chat_id: chat_id, text: text]}, %{"Content-type": "application/x-www-form-urlencoded"}) do
-      {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
-        {:ok, body}
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, "Not found"}
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
-    end
-  end
-
 end
